12410fb93061b16fd27caf73453e75b7a1dd1a25
Pranav Vashi <neobuddy89@gmail.com>
2024-07-05 22:49:25 +0530

    soong: Allow size path command for new clang
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -120,6 +120,7 @@ var Configuration = map[string]PathConfig{
 	"repo":           Allowed,
 	"rsync":          Allowed,
 	"sh":             Allowed,
+	"size":           Allowed,
 	"stubby":         Allowed,
 	"tar":            Allowed,
 	"tr":             Allowed,
